928564411bb806744d2e6fa385c0a8970a794dc5
Bojanmitic83 <bojanmitic83@gmail.com>
2021-06-02 13:06:54 +0200

    h-meni

---
+++ main.js
@@ -0,0 +1,9 @@
+//hamburger meni
+function toggleDropdown() {
+    let navbarToggle = document.getElementById("navbar-toggle");
+    if(navbarToggle.className === "topnav") {
+        navbarToggle.className += " responsive";
+    } else {
+        navbarToggle.className = "topnav"
+    }
+}
